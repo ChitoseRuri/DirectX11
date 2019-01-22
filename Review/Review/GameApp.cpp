0e3825314ac24c2484382c562522ea82d40be4dc
Chitose Ruri <1034414291@qq.com>
2019-01-22 22:23:09 +0800

    Review start

---
+++ Review/Review/GameApp.cpp
@@ -0,0 +1,119 @@
+#include "GameApp.h"
+#include "d3dUtil.h"
+#include "DXTrace.h"
+using namespace DirectX;
+using namespace std::experimental;
+
+const D3D11_INPUT_ELEMENT_DESC GameApp::VertexPosColor::inputLayout[2] = {
+	{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+};
+
+GameApp::GameApp(HINSTANCE hInstance)
+	: D3DApp(hInstance)
+{
+}
+
+GameApp::~GameApp()
+{
+}
+
+bool GameApp::Init()
+{
+	if (!D3DApp::Init())
+		return false;
+
+	if (!InitEffect())
+		return false;
+
+	if (!InitResource())
+		return false;
+
+	return true;
+}
+
+void GameApp::OnResize()
+{
+	D3DApp::OnResize();
+}
+
+void GameApp::UpdateScene(float dt)
+{
+
+}
+
+void GameApp::DrawScene()
+{
+	assert(md3dImmediateContext);
+	assert(mSwapChain);
+
+	static float black[4] = { 0.0f, 0.0f, 0.0f, 1.0f };	// RGBA = (0,0,0,255)
+	md3dImmediateContext->ClearRenderTargetView(mRenderTargetView.Get(), black);
+	md3dImmediateContext->ClearDepthStencilView(mDepthStencilView.Get(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	// 绘制三角形
+	md3dImmediateContext->Draw(3, 0);
+	HR(mSwapChain->Present(0, 0));
+}
+
+bool GameApp::InitEffect()
+{
+	ComPtr<ID3DBlob> blob;
+
+	// 创建顶点着色器
+	HR(CreateShaderFromFile(L"HLSL\\Triangle_VS.cso", L"HLSL\\Triangle_VS.hlsl", "VS", "vs_5_0", blob.ReleaseAndGetAddressOf()));
+	HR(md3dDevice->CreateVertexShader(blob->GetBufferPointer(), blob->GetBufferSize(), nullptr, mVertexShader.GetAddressOf()));
+	// 创建并绑定顶点布局
+	HR(md3dDevice->CreateInputLayout(VertexPosColor::inputLayout, ARRAYSIZE(VertexPosColor::inputLayout),
+		blob->GetBufferPointer(), blob->GetBufferSize(), mVertexLayout.GetAddressOf()));
+
+	// 创建像素着色器
+	HR(CreateShaderFromFile(L"HLSL\\Triangle_PS.cso", L"HLSL\\Triangle_PS.hlsl", "PS", "ps_5_0", blob.ReleaseAndGetAddressOf()));
+	HR(md3dDevice->CreatePixelShader(blob->GetBufferPointer(), blob->GetBufferSize(), nullptr, mPixelShader.GetAddressOf()));
+
+	return true;
+}
+
+bool GameApp::InitResource()
+{
+	// 设置三角形顶点
+	VertexPosColor vertices[] =
+	{
+		{ XMFLOAT3(0.0f, 0.5f, 0.5f), XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f) },
+		{ XMFLOAT3(0.5f, -0.5f, 0.5f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f) },
+		{ XMFLOAT3(-0.5f, -0.5f, 0.5f), XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f) }
+	};
+	// 设置顶点缓冲区描述
+	D3D11_BUFFER_DESC vbd;
+	ZeroMemory(&vbd, sizeof(vbd));
+	vbd.Usage = D3D11_USAGE_IMMUTABLE;
+	vbd.ByteWidth = sizeof vertices;
+	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vbd.CPUAccessFlags = 0;
+	// 新建顶点缓冲区
+	D3D11_SUBRESOURCE_DATA InitData;
+	ZeroMemory(&InitData, sizeof(InitData));
+	InitData.pSysMem = vertices;
+	HR(md3dDevice->CreateBuffer(&vbd, &InitData, mVertexBuffer.GetAddressOf()));
+
+
+	// ******************
+	// 给渲染管线各个阶段绑定好所需资源
+
+	// 输入装配阶段的顶点缓冲区设置
+	UINT stride = sizeof(VertexPosColor);	// 跨越字节数
+	UINT offset = 0;						// 起始偏移量
+
+	md3dImmediateContext->IASetVertexBuffers(0, 1, mVertexBuffer.GetAddressOf(), &stride, &offset);
+	// 设置图元类型，设定输入布局
+	md3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	md3dImmediateContext->IASetInputLayout(mVertexLayout.Get());
+	// 将着色器绑定到渲染管线
+	md3dImmediateContext->VSSetShader(mVertexShader.Get(), nullptr, 0);
+	md3dImmediateContext->PSSetShader(mPixelShader.Get(), nullptr, 0);
+
+
+
+
+	return true;
+}
---
+++ Review/Review/GameApp.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "d3dApp.h"
+#include "Geometry.h"
+#include "LightHelper.h"
+#include "Camera.h"
+
+class GameApp : public D3DApp
+{
+public:
+	struct VertexPosColor
+	{
+		DirectX::XMFLOAT3 pos;
+		DirectX::XMFLOAT4 color;
+		static const D3D11_INPUT_ELEMENT_DESC inputLayout[2];
+	};
+
+public:
+	GameApp(HINSTANCE hInstance);
+	~GameApp();
+
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt);
+	void DrawScene();
+
+
+private:
+	bool InitEffect();
+	bool InitResource();
+
+
+
+private:
+	ComPtr<ID3D11InputLayout> mVertexLayout;	// 顶点输入布局
+	ComPtr<ID3D11Buffer> mVertexBuffer;			// 顶点缓冲区
+	ComPtr<ID3D11VertexShader> mVertexShader;	// 顶点着色器
+	ComPtr<ID3D11PixelShader> mPixelShader;		// 像素着色器
+};
